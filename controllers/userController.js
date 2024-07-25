6fbd34f15293f1bf611dabf05e0c70fde87d5ed3
Kuncode2 <kunalasolanki36@gmail.com>
2024-07-25 10:33:28 +0530

    add secure and samsite

--- controllers/userController.js
+++ controllers/userController.js
@@ -49,6 +49,8 @@ export const logout = catchAsyncErrors(async (req, res, next) => {
     .cookie("token", "", {
       httpOnly: true,
       expires: new Date(Date.now()),
+      secure:true,
+      samSite:"None"
     })
     .json({
       success: true,
